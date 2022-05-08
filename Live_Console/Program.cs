2ab0d54c6930c4605d2d20cb180d0bf2484986b7
newreport <haruki959@qq.com>
2022-05-08 23:03:06 +0800

    禁止多开

--- Live_Console/Program.cs
+++ Live_Console/Program.cs
@@ -1,17 +1,29 @@
+using System.Diagnostics;
+
 namespace Live_Console
 {
     internal static class Program
     {
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new FrmMain());
+            Process[] processes = Process.GetProcessesByName(Application.ProductName);
+            if (processes.Length > 1)
+            {
+                MessageBox.Show("应用程序正在运行中...");
+                Environment.Exit(1);
+            }
+            else
+            {
+                // To customize application configuration such as set high DPI settings or default font,
+                // see https://aka.ms/applicationconfiguration.
+                ApplicationConfiguration.Initialize();
+                Application.Run(new FrmMain());
+            }
         }
     }
 }
